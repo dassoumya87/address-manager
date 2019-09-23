e7da6437248dd893b9f1a376354eb68ef6def49a
Soumya Ranjan Das <soumya.ranjan.das@sap.com>
2019-09-23 18:46:45 +0530

    Exercise_06_Solution

--- application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/GetAllBusinessPartnersCommand.java
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/GetAllBusinessPartnersCommand.java
@@ -1,21 +1,30 @@
 package com.sap.cloud.sdk.tutorial.addressmgr.commands;
 
 import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceConfiguration;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceDecorator;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceIsolationMode;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceRuntimeException;
 import com.sap.cloud.sdk.datamodel.odata.helper.Order;
 import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.s4hana.connectivity.DefaultErpHttpDestination;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.AddressEmailAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartnerAddress;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 
 public class GetAllBusinessPartnersCommand {
     private static final Logger logger = LoggerFactory.getLogger(GetAllBusinessPartnersCommand.class);
     private static final String CATEGORY_PERSON = "1";
     private final BusinessPartnerService businessPartnerService;
+    private final ResilienceConfiguration myResilienceConfig;
 
     private static final String DESTINATION_NAME = "S4HC_SYSTEM";
     final ErpHttpDestination destination = DestinationAccessor.getDestination(DESTINATION_NAME)
@@ -23,17 +32,49 @@ public class GetAllBusinessPartnersCommand {
 
     public GetAllBusinessPartnersCommand(final BusinessPartnerService service) {
         businessPartnerService = service;
+
+        //Resilience Configuration
+        myResilienceConfig = ResilienceConfiguration.of(BusinessPartnerService.class)
+                .isolationMode(ResilienceIsolationMode.TENANT_AND_USER_OPTIONAL)
+                .timeLimiterConfiguration(
+                        ResilienceConfiguration.TimeLimiterConfiguration.of()
+                                .timeoutDuration(Duration.ofMillis(10000)))
+                .bulkheadConfiguration(
+                        ResilienceConfiguration.BulkheadConfiguration.of()
+                                .maxConcurrentCalls(20));
+
     }
 
-    public List<BusinessPartner> execute() throws ODataException {
-        return businessPartnerService
-                .getAllBusinessPartner()
-                .select(BusinessPartner.BUSINESS_PARTNER,
-                        BusinessPartner.LAST_NAME,
-                        BusinessPartner.FIRST_NAME)
-                .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
-                .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
-                .execute(destination);
+    public List<BusinessPartner> execute() {
+        return ResilienceDecorator.executeSupplier(this::run, myResilienceConfig, e -> {
+            logger.warn("Fallback called because of exception.", e);
+            return Collections.emptyList();
+        });
+    }
 
+    private List<BusinessPartner> run() {
+        try {
+            return businessPartnerService
+                    .getAllBusinessPartner()
+                    .select(BusinessPartner.BUSINESS_PARTNER,
+                            BusinessPartner.LAST_NAME,
+                            BusinessPartner.FIRST_NAME,
+                            BusinessPartner.IS_MALE,
+                            BusinessPartner.IS_FEMALE,
+                            BusinessPartner.CREATION_DATE,
+                            BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS
+                                    .select(BusinessPartnerAddress.CITY_NAME,
+                                            BusinessPartnerAddress.COUNTRY,
+                                            BusinessPartnerAddress.TO_EMAIL_ADDRESS
+                                                    .select(AddressEmailAddress.EMAIL_ADDRESS)
+                                    )
+                    )
+                    .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
+                    .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
+                    .top(200)
+                    .execute(destination);
+        } catch (ODataException e) {
+            throw new ResilienceRuntimeException(e);
+        }
     }
 }
--- application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/GetSingleBusinessPartnerByIdCommand.java
+++ application/src/main/java/com/sap/cloud/sdk/tutorial/addressmgr/commands/GetSingleBusinessPartnerByIdCommand.java
@@ -1,5 +1,10 @@
 package com.sap.cloud.sdk.tutorial.addressmgr.commands;
 
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceConfiguration;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceDecorator;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceIsolationMode;
+import com.sap.cloud.sdk.cloudplatform.resilience.ResilienceRuntimeException;
+import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestination;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpHttpDestinationUtils;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
@@ -8,12 +13,15 @@ import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+
 public class GetSingleBusinessPartnerByIdCommand {
 
     private static final Logger logger = LoggerFactory.getLogger(GetSingleBusinessPartnerByIdCommand.class);
     private static final String CATEGORY_PERSON = "1";
 
     private final BusinessPartnerService businessPartnerService;
+    private final ResilienceConfiguration myResilienceConfig;
 
     private final String id;
 
@@ -26,25 +34,47 @@ public class GetSingleBusinessPartnerByIdCommand {
     public GetSingleBusinessPartnerByIdCommand(final BusinessPartnerService service, final String id) {
         businessPartnerService = service;
         this.id = id;
+
+        //Resilience Configuration
+        myResilienceConfig = ResilienceConfiguration.of(BusinessPartnerService.class)
+                .isolationMode(ResilienceIsolationMode.TENANT_AND_USER_OPTIONAL)
+                .timeLimiterConfiguration(
+                        ResilienceConfiguration.TimeLimiterConfiguration.of()
+                                .timeoutDuration(Duration.ofMinutes(5)))
+                .bulkheadConfiguration(
+                        ResilienceConfiguration.BulkheadConfiguration.of()
+                                .maxConcurrentCalls(20));
+
     }
 
 
-    public BusinessPartner execute() throws Exception {
-        return businessPartnerService.getBusinessPartnerByKey(id)
-                .select(BusinessPartner.BUSINESS_PARTNER,
-                        BusinessPartner.LAST_NAME,
-                        BusinessPartner.FIRST_NAME,
-                        BusinessPartner.IS_MALE,
-                        BusinessPartner.IS_FEMALE,
-                        BusinessPartner.CREATION_DATE,
-                        BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS.select(
-                                BusinessPartnerAddress.BUSINESS_PARTNER,
-                                BusinessPartnerAddress.ADDRESS_ID,
-                                BusinessPartnerAddress.COUNTRY,
-                                BusinessPartnerAddress.POSTAL_CODE,
-                                BusinessPartnerAddress.CITY_NAME,
-                                BusinessPartnerAddress.STREET_NAME,
-                                BusinessPartnerAddress.HOUSE_NUMBER))
-                .execute(destination);
+    public BusinessPartner execute() {
+        return ResilienceDecorator.executeSupplier(this::run, myResilienceConfig, e -> {
+            logger.warn("Fallback called because of exception.", e);
+            return new BusinessPartner();
+        });
     }
+
+    private BusinessPartner run() {
+        try {
+            return businessPartnerService.getBusinessPartnerByKey(id)
+                    .select(BusinessPartner.BUSINESS_PARTNER,
+                            BusinessPartner.LAST_NAME,
+                            BusinessPartner.FIRST_NAME,
+                            BusinessPartner.IS_MALE,
+                            BusinessPartner.IS_FEMALE,
+                            BusinessPartner.CREATION_DATE,
+                            BusinessPartner.TO_BUSINESS_PARTNER_ADDRESS.select(
+                                    BusinessPartnerAddress.BUSINESS_PARTNER,
+                                    BusinessPartnerAddress.ADDRESS_ID,
+                                    BusinessPartnerAddress.COUNTRY,
+                                    BusinessPartnerAddress.POSTAL_CODE, BusinessPartnerAddress.CITY_NAME,
+                                    BusinessPartnerAddress.STREET_NAME,
+                                    BusinessPartnerAddress.HOUSE_NUMBER))
+                    .execute(destination);
+        } catch (ODataException e) {
+            throw new ResilienceRuntimeException(e);
+        }
+    }
+
 }
